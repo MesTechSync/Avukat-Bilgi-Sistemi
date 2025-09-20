ea761924003b06a7e39e2d172827613eea1336e5
MesTechSync <mestechsync@example.com>
2025-09-20 22:19:03 +0300

    Vite import.meta.env ile process is not defined hatası düzeltildi.

--- Panel/src/lib/supabase.ts
+++ Panel/src/lib/supabase.ts
@@ -1,8 +1,8 @@
 import 'dotenv/config';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = process.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 async function testConnection() {
